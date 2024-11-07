a35a101f847fa4642b34405076347f15fc5dc4c7
ambervdsteen <155630578+ambervdsteen@users.noreply.github.com>
2024-11-07 09:44:14 +0100

    created search engine component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
 
 import './App.css';
+import SearchEngine from './SearchEngine.js';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-      
+
         <p>
         wilkommen to the dictionary app
       
         </p>
      
       </header>
+      <SearchEngine/>
+
+      <footer>
+        this page was built by Amber van der Steen, open-sourced on Github, and hosted on Netlify
+      </footer>
     </div>
   );
 }
---
+++ src/SearchEngine.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function SearchEngine(){
+    return (
+        <div className="container">
+           <form>search here </form> 
+        </div>
+    )
+}
+
+export default SearchEngine;
