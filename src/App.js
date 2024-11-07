20505739d479fde48a4fdfc4c1d420b0ba772530
ambervdsteen <155630578+ambervdsteen@users.noreply.github.com>
2024-11-07 08:14:11 +0100

    set up base structure

---
+++ src/App.js
@@ -0,0 +1,19 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      
+        <p>
+        wilkommen to the dictionary app
+      
+        </p>
+     
+      </header>
+    </div>
+  );
+}
+
+export default App;
