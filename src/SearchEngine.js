33a1a32eccba96006f39debcb94e47ccc460f97e
ambervdsteen <155630578+ambervdsteen@users.noreply.github.com>
2024-11-12 14:49:39 +0100

    added axios and API integration

--- src/SearchEngine.js
+++ src/SearchEngine.js
@@ -1,11 +1,20 @@
 import React, {useState} from 'react';
+import axios from 'axios';
 
 function SearchEngine(){
 const [searchTerm, setSearchTerm]= useState("");
 
+function handleResponse(response){
+    console.log(response.data.meanings[0]);
+}
+
     function search(e){
         e.preventDefault();
-        alert(`Looking up ${searchTerm}`);
+        const apiKey="2950072abb4303db56f019dto24c1aca";
+        let apiUrl=`https://api.shecodes.io/dictionary/v1/define?word=${searchTerm}&key=${apiKey}`;
+      
+    
+        axios.get(apiUrl).then(handleResponse)
     }
 
     function handleSearchTermChange(e){
